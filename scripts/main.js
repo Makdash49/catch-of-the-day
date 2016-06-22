bcb27df22fbaf36534728e24fc44b36882db2743
Mark McQuillen <markdmcq@hotmail.com>
2016-06-21 19:15:33 -0700

    finish lesson 5

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,67 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /*
+	App
+*/
+
+var App = React.createClass({
+	render : function(){
+		return (
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header />
+				</div>
+				<Order/>
+				<Inventory/>
+			</div>
+			)
+	}
+});
+
+/*
+	Header
+	<Header/>
+*/
+var Header = React.createClass({
+	render : function() {
+		return (
+			<p>Header</p>
+			)
+	}
+})
+
+/*
+	Order
+	<Order/>
+*/
+var Order = React.createClass({
+	render : function() {
+		return (
+			<p>Order</p>
+			)
+	}
+})
+
+/*
+	Inventory
+	<Inventory/>
+*/
+var Inventory = React.createClass({
+	render : function() {
+		return (
+			<p>Inventory</p>
+			)
+	}
+})
+
+
+/*
 	StorePicker
 	This will let us make <StorePicker/>
 */
-
 var StorePicker = React.createClass({
 
 	render : function() {
-	var name = "wesssssss";
 		return (
 			<form className="store-selector">
 				<h2>Please Enter A Store</h2>
@@ -21,7 +74,7 @@ var StorePicker = React.createClass({
 
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
 
 
 
