7a1f006b960a80a5da2146711b6c78922964decf
Mark McQuillen <markdmcq@hotmail.com>
2016-07-03 15:45:21 -0700

    finish lesson 25 but it doesn't work because tutorial is out of date

--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -12,10 +12,13 @@ class StorePicker extends React.Component {
 	goToStore(event) {
 		event.preventDefault();
 		// get the data from the input
+		console.log(this);
 		var storeId = this.refs.storeId.value;
-		this.history.pushState(null, '/store/' + storeId);
+		//this.history.pushState(null, '/store/' + storeId);
+		this.transitionTo('/store/' + storeId);
 	}
 	render() {
+		console.log(this);
 		return (
 			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter A Store</h2>
